2e214861ebce73ee9e7483811cb7720f541a5af8
Sfahh <stan2byc@gmail.com>
2024-03-20 20:12:49 +0100

    Menu component + landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ReportComponent } from './components/report/report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'create', component: ReportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
