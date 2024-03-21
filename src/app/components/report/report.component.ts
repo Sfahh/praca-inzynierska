193c17cf8cb2b03c3da7046e22b4d511adc7245a
Sfahh <stan2byc@gmail.com>
2024-03-21 21:55:36 +0100

    Report service created

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReportService } from '../../shared/report.service';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss'
 })
-export class ReportComponent {
+export class ReportComponent implements OnInit{
+
+  constructor(public reportService: ReportService){
+
+  }
+
+  ngOnInit(): void {
+    this.reportService.test()
+  }
+
+  downloadPDF(){
+    this.reportService.downloadPDF()
+  }
+
+  openPDF(){
+    this.reportService.openPDF()
+  }
+
+  printPDF(){
+    this.reportService.printPDF()
+  }
 
 }
