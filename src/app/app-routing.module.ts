f83c26a77736a1204f3d553e5ebfe58aed85143d
Sfahh <stan2byc@gmail.com>
2024-07-22 12:34:27 +0200

    Norma pn-en 61000-4-2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,23 @@ import { LandingPageComponent } from './components/landing-page/landing-page.com
 import { ReportComponent } from './components/report/report.component';
 import { ObjectDescriptionComponent } from './components/object-description/object-description.component';
 import { ResearchResultComponent } from './components/research-result/research-result.component';
+import { NormComponent } from './components/norms/norm/norm.component';
+import { PnEn42Component } from './components/norms/pn-en42/pn-en42.component';
 
 const routes: Routes = [
-  {path: '', component: LandingPageComponent},
-  {path: 'create', component: ReportComponent},
-  {path: 'object', component: ObjectDescriptionComponent},
-  {path: 'result', component: ResearchResultComponent}
+  { path: '', component: LandingPageComponent },
+  { path: 'create', component: ReportComponent },
+  { path: 'object', component: ObjectDescriptionComponent },
+  { path: 'result', component: ResearchResultComponent },
+  {
+    path: 'norm',
+    component: NormComponent,
+    children: [{ path: 'pn42', component: PnEn42Component }],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
